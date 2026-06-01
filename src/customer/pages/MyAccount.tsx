60b35a0c2a45f5b1e63c31366a76116176b0a43f
Tran Dong <nqtr101103@gmail.com>
2026-06-01 11:03:04 +0700

    update responsive ui

--- src/customer/pages/MyAccount.tsx
+++ src/customer/pages/MyAccount.tsx
@@ -229,7 +229,7 @@ useEffect(()=>{
 
         <div className="
           grid
-          grid-cols-3
+          grid-cols-1 xl:grid-cols-3
           gap-8
         ">
 
@@ -419,7 +419,7 @@ useEffect(()=>{
 
             <div className="
               grid
-              grid-cols-4
+              grid-cols-1 md:grid-cols-2 xl:grid-cols-4
               gap-6
             ">
 
